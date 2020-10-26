c20f2c91620476c96ed1a2b9d23b74c6940f154d
HyunCheolNam <guscjfrltk@naver.com>
2020-10-26 17:26:56 +0900

    practice

--- gui_basic/6_checkbox.py
+++ gui_basic/6_checkbox.py
@@ -8,7 +8,7 @@ chkvar = IntVar() #chkvar에 인트형으로 값을 저장한다
 chkbox = Checkbutton(root, text="오늘 하루 보지 않기", variable=chkvar)
 chkbox.select()#자동선택처리
 chkbox.deselect()#선택 해제 처리
-chkbox.pack()
+chkbox.pack()#팩은 필수
 
 def btncmd():
     pass
